f7f30347394fa417d33a95c1ea072154749229d1
Qofy <148817944+Qofy@users.noreply.github.com>
2025-05-18 20:50:36 +0200

    working on the skills sections

--- src/component/Nav.tsx
+++ src/component/Nav.tsx
@@ -3,9 +3,10 @@ import { useState, useEffect } from 'react';
 import "../styles/component/nav.scss";
 
 type NavProps = {
-  nav:string
+  nav: string
 }
-export function Nav({nav}:NavProps) {
+
+export function Nav({nav}: NavProps) {
   const [activeSection, setActiveSection] = useState('home');
 
   useEffect(() => {
@@ -42,10 +43,34 @@ export function Nav({nav}:NavProps) {
     <nav className={nav}>
       <ul>
         <li>
-          <HashLink smooth className={activeSection === 'about' ? 'link active' : 'link'} to="#about">About</HashLink>
-          <HashLink smooth className={activeSection === 'skills' ? 'link active' : 'link'} to="#skills">Skills</HashLink>
-          <HashLink smooth className={activeSection === 'projects' ? 'link active' : 'link'} to="#projects">Projects</HashLink>
-          <HashLink smooth className={activeSection === 'contact' ? 'link active' : 'link'} to="#contact">Contact Me</HashLink>
+          <HashLink 
+            smooth 
+            className={activeSection === 'about' ? 'link active' : 'link'} 
+            to="#about"
+          >
+            About
+          </HashLink>
+          <HashLink 
+            smooth 
+            className={activeSection === 'skills' ? 'link active' : 'link'} 
+            to="#skills"
+          >
+            Skills
+          </HashLink>
+          <HashLink 
+            smooth 
+            className={activeSection === 'projects' ? 'link active' : 'link'} 
+            to="#projects"
+          >
+            Projects
+          </HashLink>
+          <HashLink 
+            smooth 
+            className={activeSection === 'contact' ? 'link active' : 'link'} 
+            to="#contact"
+          >
+            Contact Me
+          </HashLink>
         </li>
       </ul>
     </nav>
--- src/component/Skill.tsx
+++ src/component/Skill.tsx
@@ -1,46 +1,84 @@
 import { SectionContainer } from "../sub-components/SectionContainer"
 import "../styles/component/skills.scss"
-import { FaHtml5, FaSass, FaCss3 } from "react-icons/fa";
+import { FaHtml5, FaSass, FaCss3, FaReact, FaJs,FaFigma,FaGitAlt,FaJava,FaPython,FaJira } from "react-icons/fa";
+import { SiTypescript,SiCplusplus,SiFlutter } from "react-icons/si";
 
-export function Skills (){
-  const skillContent=(
+export function Skills() {
+  const skillContent = (
     <section className="section-main__container">
-        <h1>
-          My Skills
-        </h1>
-        <SkillsContent/>
+      <h1>My Skills</h1>
+      <SkillsContent />
     </section>
   )
-  return(
-    <SectionContainer children={skillContent}/>
+  
+  return (
+    <SectionContainer children={skillContent} />
   )
 }
 
-export function SkillsContent(){
-  return(
-    <div>
-      <h4>
-        Using Now
-      </h4>
-      <div>
-        <div>
-        <FaHtml5/>
-        HTML
+export function SkillsContent() {
+  return (
+    <div className="skills-main">
+      <h4>Using Now :</h4>
+      <div className="skills-grid">
+        <div className="skill-item">
+          <FaHtml5 className="skill-icon" />
+          <span>HTML</span>
         </div>
-      <div>
-      <FaSass/>
-        SASS
+        <div className="skill-item">
+          <FaCss3 className="skill-icon" />
+          <span>CSS</span>
+        </div>
+        <div className="skill-item">
+          <FaSass className="skill-icon" />
+          <span>SASS</span>
+        </div>
+        <div className="skill-item">
+          <FaJs className="skill-icon" />
+          <span>JavaScript</span>
+        </div>
+        <div className="skill-item">
+          <FaReact className="skill-icon" />
+          <span>React</span>
+        </div>
+        <div className="skill-item">
+          <SiTypescript className="skill-icon" />
+          <span>Typescript</span>
+        </div>
+        <div className="skill-item">
+          <FaFigma className="skill-icon" />
+          <span>Figma</span>
+        </div>
+        
       </div>
-      <div>
-      <FaCss3/>
-        Css
+      
+      <div className="skills-grid">
+      <div className="skill-item">
+          <FaJava className="skill-icon" />
+          <span>Java</span>
+        </div>
+        <div className="skill-item">
+          <FaPython className="skill-icon" />
+          <span>Python</span>
+        </div>
+        <div className="skill-item">
+          <SiCplusplus className="skill-icon" />
+          <span>C++</span>
+        </div>
+        <div className="skill-item">
+          <SiFlutter className="skill-icon" />
+          <span>Flutter</span>
+        </div>
+        <div className="skill-item">
+          <FaJira className="skill-icon" />
+          <span>Jira</span>
+        </div>
+        <div className="skill-item">
+          <FaGitAlt className="skill-icon" />
+          <span>Git</span>
+        </div>
       </div>
-      <div>
-      <FaSass/>
-        SASS
-      </div>
-      </div>
-  
+     
     </div>
   )
 }
