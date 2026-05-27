08585f56c4da01466a673574aeb25075bc4f5c23
Qofy <safopencilart@icloud.com>
2026-05-27 08:16:46 +0200

    chore: comment out unused course listings in background data

--- src/data/background.ts
+++ src/data/background.ts
@@ -1,14 +1,14 @@
 
 export const uniCourses = {
   semester1:["Programming(Python)", "Computer Architecture", "Software Engineering1","Mathematics and Statistics","Business Employability In English","Management Bases"],
-  semester2:["Big Data Aanaylsis", "Start-up Management", "Parallel Programming","Big Data Analytics","Back-end Development","Digital And Media Communication"],
+  // semester2:["Big Data Aanaylsis", "Start-up Management", "Parallel Programming","Big Data Analytics","Back-end Development","Digital And Media Communication"],
   semester3:["IT Platforms", "Software Engineering 2","Cloud Computing & Distributed Systems", "Operating System", "Software Quality Assurance","GUI Design & Web Optimization"],
   semester4:["Artificial Intelligence(AI)", "Frontend Developer(Using Flutter, HTML,Javascript, CSS and Node.js)","IT Agail", "Coperate Management", "Machine Learning & Smart Systems", "IT Security"]
 }
 export const seniorHCourses= {
   coreSubjects:{
       header:"Responsibilities",
-      courses:["Developed and maintained web applications using modern frontend", "Implemented backend services using Node.js, NestJS, Python, and Rust For APIs.","Built user interfaces using React, Nextjs, and Svelte.", "Used Git and GitLab for version control and collaboration.", "Tested and debugged applications to ensure reliability and performance.","Learned and applied professional software development workflows.","Used AI tools responsibly to design, implement, and troubleshoot software solutions."],
+      // courses:["Developed and maintained web applications using modern frontend", "Implemented backend services using Node.js, NestJS, Python, and Rust For APIs.","Built user interfaces using React, Nextjs, and Svelte.", "Used Git and GitLab for version control and collaboration.", "Tested and debugged applications to ensure reliability and performance.","Learned and applied professional software development workflows.","Used AI tools responsibly to design, implement, and troubleshoot software solutions."],
   },
   electiveSubject:{
     header:"Achievements",
